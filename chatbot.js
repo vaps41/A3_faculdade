682c898ababc7191013c5d30170a9ef831a7809d
Victor Andrade <65470967+vaps41@users.noreply.github.com>
2021-11-11 01:27:26 -0300

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,17 @@
 var hoje = new Date().getFullYear();
 document.getElementById("hoje").innerHTML = hoje;
 
+function interesse(){
+    var block= document.getElementById("chat").style.display ="block";
+
+    if(block != true){
+        document.getElementById("chat").style.display ="block";
+        console.log("estou aqui");
+        return true;
+    }else{
+        document.getElementById("chat").style.display ="none";
+        console.log("sai vazado");
+        return false;
+    }
+}
+
